0cfdd885862f46bb19b48b5afd97f6fd2a192d39
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-07-06 13:14:52 -0700

    need to add heart functionality

---
+++ js/challenge.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const counter = document.getElementById("counter")
+    const plus = document.getElementById("plus")
+    const minus = document.getElementById("minus")
+    const heart = document.getElementById("heart")
+
+    const second = () => {
+        counter.innerText = parseInt(counter.innerText,  10) + 1
+    }
+
+    const subSecond = () => {
+        counter.innerText = parseInt(counter.innerText, 10) - 1
+    }
+
+
+let count = setInterval(second, 1000)
+
+plus.addEventListener("click", function(){
+    const a = document.getElementById("counter")
+    const b = parseInt(a.innerText)
+    a.innerText=b+1
+})
+
+minus.addEventListener("click", function(){
+    const a = document.getElementById("counter")
+    const b= parseInt(a.innerText)
+    a.innerText=b-1
+})
+
+let i = 0
+
+heart.addEventListener("click", function(){
+    const counter = document.getElementById("counter")
+    const parse = parseInt(counter.innerText)
+
+    const createLi = document.createElement("li")
+    const listLi = document.querySelector(".likes")
+
+    createLi.textContent = `${parse} has been liked  ${i} times`
+
+    listLi.appendChild(createLi)
+    console.log(listLi)
+
+    })
+
+})
+
