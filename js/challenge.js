a45bf5ada400b962acd4afd8a32f1c87b086ce9c
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-07-07 15:03:57 -0700

    assignment completed

--- js/challenge.js
+++ js/challenge.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function(){
     const plus = document.getElementById("plus")
     const minus = document.getElementById("minus")
     const heart = document.getElementById("heart")
+    const pause = document.getElementById("pause")
+    const commentForm = document.getElementsByTagName("form")[0]
 
     const second = () => {
         counter.innerText = parseInt(counter.innerText,  10) + 1
@@ -11,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function(){
     const subSecond = () => {
         counter.innerText = parseInt(counter.innerText, 10) - 1
     }
-
 
 let count = setInterval(second, 1000)
 
@@ -27,17 +28,73 @@ minus.addEventListener("click", function(){
     a.innerText=b-1
 })
 
-let i = 0
+const arr = []
 
 heart.addEventListener("click", function(){
     const counter = document.getElementById("counter")
     const parse = parseInt(counter.innerText)
+    let objcounter
+    if(arr.find(obj => obj.counter === parse)){
+        objcounter = arr.find(obj => obj.counter === parse)
+        objcounter.i++       
+    } else {
+     objcounter = {
+        counter: parse,
+        i: 1
+        }
+        arr.push(objcounter)
+    }
     const createLi = document.createElement("li")
     const listLi = document.querySelector(".likes")
-
-    createLi.textContent = `${parse} has been liked ${i} times`
+    createLi.textContent = `${parse} has been liked ${objcounter.i} times`
     listLi.appendChild(createLi)
     })
+
+pause.addEventListener("click", function(){
+    // const buttons = document.getElementsByTagName(`.button`)
+    // console.log(buttons)
+    // if(buttons.disabled === false){
+    //     clearInterval(count)
+    //     buttons.disabled = true
+    //     console.log("hello")
+    // } else {
+    //     buttons.disabled = false
+    //     console.log("bye")
+    // }
+    // clearInterval(count)
+
+    if(plus.disabled === false && minus.disabled === false && heart.disabled === false && pause.disabled === false){
+        clearInterval(count)
+        plus.disabled = true
+        minus.disabled = true
+        heart.disabled = true
+        pause.innerHTML = "resume"
+    } else {
+        plus.disabled = false
+        minus.disabled = false
+        heart.disabled = false
+        pause.diabled = false
+        if(pause.innerHTML === "resume"){
+        pause.innerHTML = "pause"
+        setInterval(second, 1000)
+    } else if (pause.innerHTML === "pause"){
+        pause.innerHTML = "pause"
+        clearInterval(count)
+    }
+    }  
+    
+})
+
+commentForm.addEventListener("submit", function(a){
+    a.preventDefault()
+    let b = this.children[0]
+    c = b.value; b.value = ""
+    var d = document.querySelector(".comments")
+    e = document.createElement("p")
+    e.innerText=c,d.appendChild(e)
+
+})
+
 
 })
 
